ae37adb04ce7dbfa3b198c451f26a5f6ec8fdd24
sashashlemkevych <sashashlemcevich@gmail.com>
2024-04-23 14:11:07 +0300

    LR4

--- mysite/app_blog/models.py
+++ mysite/app_blog/models.py
@@ -9,16 +9,31 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
-    slug = models.SlugField(default='default-slug')
+    category = models.CharField(u'Категорія', 
+        max_length=250, help_text=u'Максимум 250 символів')
+    slug = models.SlugField(u'Слаг') 
+    objects = models.Manager()
+ 
+    class Meta:
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікацій'
 
+    def __str__ (self): 
+        return self.category
 
-class Meta:
-    verbose_name = u'Категорія для публікації’'
-    verbose_name_plural = u'Категорії для публікації’'
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/" 
+        return url
 
-def   str (self): 
-    return self.category
+    class Meta:
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікації'
+
+    def __str__ (self): 
+        return self.category
  
 class Article(models.Model):
     title = models.CharField(u'Заголовок', max_length=250,
@@ -31,20 +46,20 @@ class Article(models.Model):
     category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
     objects = models.Manager()
 
-class Meta:
-    ordering = ['-pub_date'] 
-    verbose_name = u'Стаття' 
-    verbose_name_plural = u'Статті'
+    class Meta:
+        ordering = ['pub_date'] 
+        verbose_name = u'Стаття' 
+        verbose_name_plural = u'Статті'
 
-def str (self): 
-    return self.title
+    def __str__ (self): 
+       return self.title
 
-def get_absolute_url(self):
-    try:
-        url = reverse('news-detail', kwargs={'year': self.pub_date.strftime("%Y"), 'month': self.pub_date.strftime("%m"), 'day': self.pub_date.strftime("%d"), 'slug': self.slug,})
-    except:
-        url = "/" 
-    return url
+    def get_absolute_url(self):
+        try:
+            url = reverse('news-detail', kwargs={'year': self.pub_date.strftime("%Y"), 'month': self.pub_date.strftime("%m"), 'day': self.pub_date.strftime("%d"), 'slug': self.slug,})
+        except:
+            url = "/" 
+        return url
  
 class ArticleImage(models.Model): 
     article = models.ForeignKey(Article,verbose_name=u'Стаття', related_name='images', on_delete=models.CASCADE)
@@ -52,12 +67,12 @@ class ArticleImage(models.Model):
     title = models.CharField(u'Заголовок', max_length=250,
     help_text=u'Максимум 250 символів', blank=True)
 
-class Meta:
-    verbose_name = u'Фото для статті'
-    verbose_name_plural = u'Фото для статті'
+    class Meta:
+        verbose_name = u'Фото для статті'
+        verbose_name_plural = u'Фото для статті'
 
-def str (self): 
-    return self.title
+    def __str__ (self): 
+        return self.title
 
 @property
 def filename(self):
